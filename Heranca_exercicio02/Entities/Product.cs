05cdd47336de22a12bcf2456156f2f14473a1107
Diego Leite <diego@DESKTOP-ME841EH>
2022-02-03 21:48:06 -0300

    Classe Produto com metodos

---
+++ Heranca_exercicio02/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heranca_exercicio02.Entities {
+    class Product {
+        public string Name { get; set; }
+        public double  Price { get; set; }
+
+        public Product(string name, double price) {
+            Name = name;
+            Price = price;
+        }
+
+        public string PriceTag() {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Name);
+            sb.AppendLine("R$ " + Price);
+            return sb.ToString();
+        }
+    }
+}
