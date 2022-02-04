984771a0a1f45a949280099232f3cf4753dba814
Diego Leite <diego@DESKTOP-ME841EH>
2022-02-03 22:01:14 -0300

    Criacao classe UsedProduct com metodos

---
+++ Heranca_exercicio02/Entities/UsedProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heranca_exercicio02.Entities {
+    class UsedProduct : Product{
+        
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate) : base(name, price) {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag() {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Name + " (Used) - R$" + Price + " (Manufacture Date: " + ManufactureDate);            
+            
+            return sb.ToString();
+        }
+    }
+}
